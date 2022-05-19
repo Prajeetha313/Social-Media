828f73467b98ece94c96f3b5702648ececfc6de2
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-05-19 17:12:09 +0530

    Add files via upload

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -3,12 +3,29 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next){
     const token = req.header('auth-token');
+    console.log(token)
     if(!token) return res.status(401).json("Access Denied");
 
     try{
-        const verified = jwt.verify(token, process.env.JWT_KEY);
-        req.user = verified;
-        next()
+        const verified = jwt.verify(token, process.env.JWT_KEY, function(err, decode){
+            if(err){
+                return res.json({success:false, message : "Login expires!!! Login Again to continue"})
+            }
+            else
+            {
+                if(req.session.auth){
+    
+                next()
+                }
+                else
+                {
+                    req.session.error = "Login again";
+                    res.json({message : "login again to continue"})
+                }
+            }
+        });
+        
+        
     }catch(err){
         res.status(400).json("Invalid Token")
     }
