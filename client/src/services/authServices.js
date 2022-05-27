7e0e14ecdef3ed6abce301d74752f8ab237a2235
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-05-27 09:38:58 +0530

    Update authServices.js

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,22 +1,19 @@
 import axios from 'axios';
 
 const API_URL = "http://localhost:8800/api/auth/";
-const login = (email, password) => {
-    return axios
+const login = async (email, password) => {
+    const res = await axios
         .post(API_URL + 'login', {
             email,
             password
         })
-        .then((res)=> {
-            console.log(res.data.user[0])
-            if(res.data.token) {
-                localStorage.setItem("token", JSON.stringify(res.data.token));
-                localStorage.setItem("user", JSON.stringify(res.data.user[0]._id));
-                localStorage.setItem("username", JSON.stringify(res.data.user[0].username));
-                window.location.replace('/Home')
-            }
-            return res.data;
-        });
+        if(res.data.token) {
+            localStorage.setItem("token", JSON.stringify(res.data.token));
+            localStorage.setItem("user", JSON.stringify(res.data.user[0]._id));
+            localStorage.setItem("username", JSON.stringify(res.data.user[0].username));
+            window.location.replace('/Home')
+        }
+        return res.data;
 };
 
 const logout = () => {
@@ -24,6 +21,18 @@ const logout = () => {
     localStorage.removeItem("user");
     localStorage.removeItem("username");
     window.location.replace("/Login")
+};
+
+const register = async (username, email, password, mobile) => {
+    const res = await axios
+        .post(API_URL + 'register', {
+            username, 
+            email, 
+            password, 
+            mobile
+        })
+        window.location.replace("/Login")
+        return res.data
 }
 
-export default { login, logout };
+export default { login, logout, register };
