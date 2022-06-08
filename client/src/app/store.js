2da13a5329aa053aa09f5b7aa25f44c1c4d012b4
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-06-08 12:29:27 +0530

    Add files via upload

---
+++ client/src/app/store.js
@@ -0,0 +1,35 @@
+import userReducer from '../reducer/userSlice';
+import postReducer from '../reducer/postSlice';
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+import {
+    persistStore,
+    persistReducer, 
+    REGISTER, 
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key : "root", 
+    version : 1, 
+    storage, 
+};
+
+const rootReducer = combineReducers({
+    user : userReducer, 
+    post : postReducer, 
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer : persistedReducer, 
+    middleware : (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+            serializableCheck : {
+                ignoreActions : [REGISTER], 
+            }, 
+        }),
+});
+
+export let persistor = persistStore(store);
